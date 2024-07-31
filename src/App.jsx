e63f9cb2dec32d6e78d0e0c97a814e07d5e6ee4c
erfan <erfanghezi5050@gmail.com>
2024-07-31 21:37:47 +0330

    add : add file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+        <p className='text-red-600 text-center items-center font-bold'>Salam be proje jadad v bozorg react</p>
+        <h2 className="text-center text-blue-500">Erfan</h2>
+    </>
+  )
+}
+
+export default App
