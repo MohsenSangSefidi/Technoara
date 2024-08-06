355bc3670d0c1de50c5d69f45a96b7ec70e1547c
erfan <erfanghezi5050@gmail.com>
2024-08-06 09:46:54 +0330

    Add page

---
+++ src/pages/Index/Index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from "../../components/Header/Header"
+import Banner from '../../components/Banner/Banner'
+import CategoryBox from '../../components/CategoryBox/CategoryBox'
+import DiscountedCarts from '../../components/ِDiscountedCarts/DiscountedCarts'
+import PopularCategory from '../../components/PopularCategory/PopularCategory'
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <main className='mt-10 px-4 w-full py-4'>
+        <Banner/>
+        <div className='flex flex-col xl:flex-row mt-20 items-start h-auto xl:h-[25.5rem] justify-between'>
+          <DiscountedCarts/>
+          <CategoryBox/>
+        </div>
+        <PopularCategory/>
+        <br />
+        <br />
+        <br />
+        <br />
+      </main>
+    </>
+  )
+}
