30071624c88032f50bfc592ad8178d2ee83051b1
erfan <erfanghezi5050@gmail.com>
2024-08-07 16:27:31 +0330

    Add styles

--- src/components/TitleBox/TitleBox.jsx
+++ src/components/TitleBox/TitleBox.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function TitleBox({title,link}) {
   return (
     <div className='flex mt-10 items-center justify-between'>
-       <p className='font-YekanBakh font-bold text-secondary text-[1.5rem]'>{title}</p>
-       <Link className='font-YekanBakh font-bold text-secondary' to={"#"}>{link}</Link>
+       <p className='font-YekanBakh font-bold text-secondary text-[1.1rem] md:text-[1.5rem]'>{title}</p>
+       <Link className='font-YekanBakh font-bold text-secondary text-[0.9rem] md:text-[1.1rem]' to={"#"}>{link}</Link>
     </div>
   )
 }
