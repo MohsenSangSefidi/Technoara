6a05b23d11c04c7b7b5b7fb6407bafa488017ba4
erfan <erfanghezi5050@gmail.com>
2024-08-07 16:26:53 +0330

    Add responsive breakpoints in swiper

--- src/components/PopularCategory/PopularCategory.jsx
+++ src/components/PopularCategory/PopularCategory.jsx
@@ -2,28 +2,41 @@ import React from "react";
 import TitleBox from "../../components/TitleBox/TitleBox";
 import PopularCategoryBox from "./PopularCategoryBox/PopularCategoryBox";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useSwiper } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import SlideButtons from "../SlideButtons/SlideButtons";
 
 import "swiper/css";
 import 'swiper/css/navigation';
 
 export default function PopularCategory() {
-
-  const swiper = useSwiper();
-
   return (
-    <>
+    <section className="mt-20 xl:mt-10">
       <TitleBox title={"دسته بندی های محبوب"} link={"مشاهده همه"} />
-      <div className="flex items-center justify-between relative mt-12 w-[90%] mx-auto">
+        <div className="flex items-center justify-between relative mt-12 w-[90%] mx-auto">
         <Swiper
           direction="horizontal"
+          className="p-3"
           modules={[Navigation]}
           navigation={{
             nextEl : ".swiper-button-next",
             prevEl : ".swiper-button-prev"
           }}
-          slidesPerView={9}
+          slidesPerView={2}
+          breakpoints={{
+            576: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+            1200: {
+              slidesPerView: 8,
+              width:1500
+            },
+          }}
         >
           <SwiperSlide>
             <PopularCategoryBox />
@@ -61,10 +74,9 @@ export default function PopularCategory() {
           <SwiperSlide>
             <PopularCategoryBox />
           </SwiperSlide>
-          <button onClick={() => swiper.slideNext()} className="swiper-button-next">next</button>
-          <button onClick={() => swiper.slidePrev()} className="swiper-button-prev">pre</button>
+          <SlideButtons />
         </Swiper>
       </div>
-    </>
+    </section>
   );
 }
