ee81e7c9ff4a76236c293f98512b44f876570d70
Mohsen <sangsefidimohsen84@gmail.com>
2024-08-09 09:40:12 +0330

    Config Create Comment Api

--- Product_Module/urls.py
+++ Product_Module/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProductApiView, ProductDetailApiView, CreateProductApiView, CreateProductFeatureApiView, CreateProductImagesApiView
+from .views import (ProductApiView, ProductDetailApiView, CreateProductApiView, CreateProductFeatureApiView,
+                    CreateProductImagesApiView, CreateCommentApiView)
 
 urlpatterns = [
     path('filter/', ProductApiView.as_view()),
-    path('create/', CreateProductApiView.as_view()),
+    path('create-product/', CreateProductApiView.as_view()),
+    path('create-comment/', CreateCommentApiView.as_view()),
     path('create-feature/', CreateProductFeatureApiView.as_view()),
     path('create-images/', CreateProductImagesApiView.as_view()),
     path('detail/<int:pk>/', ProductDetailApiView.as_view())
