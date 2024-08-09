6e1045d54739c7e6869ba506a73e9b23c3c6ab2f
erfan <erfanghezi5050@gmail.com>
2024-08-09 11:21:39 +0330

    Add components

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -6,11 +6,14 @@ import DiscountedCarts from '../../components/ِDiscountedCarts/DiscountedCarts'
 import PopularCategory from '../../components/PopularCategory/PopularCategory'
 import NewProducts from '../../components/NewProducts/NewProducts'
 import Mobile from '../../components/Mobiles/Mobile'
+import BestSellingProducts from '../../components/BestSellingProducts/BestSellingProducts'
+import LapTop from '../../components/LapTops/LapTop'
+import Articles from '../../components/Articles/Articles'
 
 export default function Index() {
   return (
     <>
-      <Header />
+      <Header/>
       <main className='mt-10 px-4 w-full py-4'>
         <Banner/>
         <div className='flex flex-col xl:flex-row mt-20 items-start h-auto xl:h-[25.5rem] justify-between'>
@@ -20,13 +23,9 @@ export default function Index() {
         <PopularCategory/>
         <NewProducts />
         <Mobile/>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <BestSellingProducts/>
+        <LapTop/>
+        <Articles/>
         <br />
       </main>
     </>
