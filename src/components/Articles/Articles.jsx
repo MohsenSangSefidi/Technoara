3c48a2a07720199d5c397cf24f7f8f363c4eb284
erfan <erfanghezi5050@gmail.com>
2024-08-14 19:24:55 +0330

    Updated this component

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -4,10 +4,10 @@ import ArticleBox from './ArticleBox/ArticleBox'
 
 export default function Articles() {
   return (
-    <section className='mt-20'>
+    <section className='mt-0 md:mt-20'>
       <TitleBox title={"وبلاگ"} link={"مشاهده"}/>
-       <div className='flex items-center h-[27rem] justify-center gap-6 mt-10'>
-          <div className='w-[55%] relative h-[100%]'>
+       <div className='flex flex-col lg:flex-row items-center h-auto lg:h-[27rem] justify-center gap-6 mt-10'>
+          <div className='w-full lg:w-[55%] lg:mt-[5rem] xl:mt-0 relative h-[23rem] lg:h-[100%]'>
            <div className='w-[100%] h-[100%] overflow-hidden rounded-2xl'>
             <img src="public/images/articles/3-11.jpg" className='w-full h-full' alt="" />
            </div>
@@ -15,7 +15,7 @@ export default function Articles() {
                <div className='flex flex-col'>
                   <p className='font-YekanBakh text-white'>اندروید باکس نت باکس موبوادیشن ، تلویزیون هوشمند ارزان</p>
                </div>
-               <div className='flex items-center text-white gap-8 justify-start'>
+               <div className='flex flex-wrap md:flex-nowrap items-center text-white gap-8 justify-start'>
                    <div className='flex items-center justify-center gap-2'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
@@ -43,7 +43,7 @@ export default function Articles() {
                </div>
            </div>
           </div>
-          <div className='w-[45%] flex flex-col gap-7 h-[100%] items-start justify-start'>
+          <div className='w-full lg:w-[45%] mt-5 flex flex-col gap-7 h-[100%] items-start justify-start'>
               <ArticleBox />
               <ArticleBox />
               <ArticleBox />
