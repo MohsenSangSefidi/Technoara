5cf155298dc9e632c28d7aca577c7f78f66fead4
Mohsen <sangsefidimohsen84@gmail.com>
2024-08-15 20:25:15 +0330

    Change README.md

--- User_Module/views.py
+++ User_Module/views.py
@@ -15,7 +15,6 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
-
 class GetUserAPIView(RetrieveAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -39,9 +38,11 @@ class RegisterUserAPIView(APIView):
             last_name = serializer.validated_data.get('last_name')
             password = serializer.validated_data.get('password')
             email = serializer.validated_data.get('email')
-            check_user = UserModel.objects.filter(username=username, email=email).first()
+            check_user = UserModel.objects.filter(username=username, email=email.lower()).first()
             if check_user is None:
-                user = UserModel(username=username, first_name=first_name, last_name=last_name, email=email, is_active=False, user_token=get_random_string(50), user_verify_code=verify_code(), user_avatar=avatar)
+                user = UserModel(username=username, first_name=first_name, last_name=last_name, email=email.lower(),
+                                 is_active=False, user_token=get_random_string(50), user_verify_code=verify_code(),
+                                 user_avatar=avatar)
                 user.set_password(password)
                 user.save()
 
@@ -70,7 +71,6 @@ class RegisterUserAPIView(APIView):
             Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class ActiveUserAPIView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -80,7 +80,7 @@ class ActiveUserAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             user_token = serializer.validated_data.get('user_token')
             verify_code = serializer.validated_data.get('verify_code')
-            user =  UserModel.objects.filter(user_token=user_token).first()
+            user = UserModel.objects.filter(user_token=user_token).first()
             if user is not None:
                 if str(user.user_verify_code) == str(verify_code):
                     user.is_active = True
@@ -103,14 +103,15 @@ class LoginUserAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             email = serializer.validated_data.get('email')
             password = serializer.validated_data.get('password')
-            user = UserModel.objects.filter(email=email).first()
+            user = UserModel.objects.filter(email=email.lower()).first()
             if user is not None:
                 if user.check_password(password):
                     if user.is_active:
                         user.user_token = get_random_string(60)
                         user.save()
                         serializer = GetUserSerializer(user)
-                        return Response({'token': user.user_token, 'user-detail' : serializer.data}, status=status.HTTP_200_OK)
+                        return Response({'token': user.user_token, 'user-detail': serializer.data},
+                                        status=status.HTTP_200_OK)
                     else:
                         return Response({'detail': 'Account Disabale'}, status=status.HTTP_403_FORBIDDEN)
                 else:
@@ -131,7 +132,7 @@ class SendVerifyCodeAPIView(APIView):
             user_token = serializer.validated_data.get('user_token')
             email = serializer.validated_data.get('email')
 
-            user = UserModel.objects.filter(user_token=user_token, email=email).first()
+            user = UserModel.objects.filter(user_token=user_token, email=email.lower()).first()
 
             if user is not None:
                 user.user_verify_code = verify_code()
@@ -146,7 +147,7 @@ class SendVerifyCodeAPIView(APIView):
                         "Verify Code",
                         f"It's your Code : {user.user_verify_code}",
                         HOST_EMAIL,
-                        [user_email],
+                        [user_email.lower()],
                         fail_silently=False,
                     )
                     detail = "Email Sent"
