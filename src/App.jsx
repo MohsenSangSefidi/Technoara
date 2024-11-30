6e96dd5c6a85bf612e12b8f8bb3a162ccc93f892
erfan <erfanghezi5050@gmail.com>
2024-11-30 09:46:08 +0330

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import routes from "./routes";
+import routes from "./routes/routes";
 import { useRoutes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+import AuthProvider from "./context/AuthContext/AuthContext";
 
 function App() {
   const router = useRoutes(routes);
 
-  return <>{router}</>;
+  const client = new QueryClient();
+
+  return (
+    <>
+      <QueryClientProvider client={client}>
+        <AuthProvider>{router}</AuthProvider>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
