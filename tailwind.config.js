73d2decebccdb4b8dd0309f203572220363b2027
erfan <erfanghezi5050@gmail.com>
2024-11-30 10:03:23 +0330

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,39 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode : ["class"],
   theme: {
-    extend: {
-      fontFamily : {
-        "Dorna" : "Dorna" ,
-        "YekanBakh" : "YekanBakh",
-        "Kalame" : "Kalame"
-      }
-      ,
-      colors : {
-        "primary" : "#AF47D2",
-        "secondary" : "#021959"
-      },
-      screens : {
-        "xs" : "368px",
-        "sm" : "576px"
-      }
-    },
+  	extend: {
+  		fontFamily: {
+  			'Dorna': 'Dorna',
+  			'YekanBakh': 'YekanBakh',
+  			'Kalame': 'Kalame'
+  		},
+  		colors: {
+  			'primary': '#AF47D2',
+  			'secondary': '#021959',
+  		},
+  		boxShadow: {
+  			'custom': '0px -4px 18px 0px #0000001c'
+  		},
+  		screens: {
+  			'xs': '368px',
+  			'sm': '576px'
+  		},
+  		keyframes: {
+  			showTab: {
+  				'0%': {
+  					opacity: '0'
+  				},
+  				'100%': {
+  					opacity: '1'
+  				}
+  			}
+  		},
+  		animation: {
+  			showTab: 'showTab 0.3s ease-in-out'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
